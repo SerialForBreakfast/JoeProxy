57af25e02bd78e33c8f9b9e1616938b1acbaa826
Joe <joe@showblender.com>
2024-06-06 14:41:26 -0700

    Initial Commit

---
+++ JoeProxy/JoeProxyApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoeProxyApp.swift
+//  JoeProxy
+//
+//  Created by Joseph McCraw on 6/6/24.
+//
+
+import SwiftUI
+
+@main
+struct JoeProxyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
