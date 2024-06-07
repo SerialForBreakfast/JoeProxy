c3863b711fc3ee3b4fd5825e225a68d31dc386bb
Joe <joe@showblender.com>
2024-06-06 17:20:40 -0700

    filtering tests

---
+++ JoeProxyTests/FilteringServiceTests.swift
@@ -0,0 +1,43 @@
+//
+//  FilteringServiceTests.swift
+//  JoeProxyTests
+//
+//  Created by Joseph McCraw on 6/6/24.
+//
+import XCTest
+@testable import JoeProxy
+
+class FilteringServiceTests: XCTestCase {
+
+    func testAllowListFiltering() {
+        let criteria = FilteringCriteria(urls: ["example.com"], filterType: .allow)
+        let filteringService = DefaultFilteringService(criteria: criteria)
+
+        XCTAssertTrue(filteringService.shouldAllowRequest(url: "https://example.com/test"))
+        XCTAssertFalse(filteringService.shouldAllowRequest(url: "https://other.com/test"))
+    }
+
+    func testBlockListFiltering() {
+        let criteria = FilteringCriteria(urls: ["example.com"], filterType: .block)
+        let filteringService = DefaultFilteringService(criteria: criteria)
+
+        XCTAssertFalse(filteringService.shouldAllowRequest(url: "https://example.com/test"))
+        XCTAssertTrue(filteringService.shouldAllowRequest(url: "https://other.com/test"))
+    }
+
+    func testEmptyAllowList() {
+        let criteria = FilteringCriteria(urls: [], filterType: .allow)
+        let filteringService = DefaultFilteringService(criteria: criteria)
+
+        XCTAssertFalse(filteringService.shouldAllowRequest(url: "https://example.com/test"))
+        XCTAssertFalse(filteringService.shouldAllowRequest(url: "https://other.com/test"))
+    }
+
+    func testEmptyBlockList() {
+        let criteria = FilteringCriteria(urls: [], filterType: .block)
+        let filteringService = DefaultFilteringService(criteria: criteria)
+
+        XCTAssertTrue(filteringService.shouldAllowRequest(url: "https://example.com/test"))
+        XCTAssertTrue(filteringService.shouldAllowRequest(url: "https://other.com/test"))
+    }
+}
